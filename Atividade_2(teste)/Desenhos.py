eccf915214ada8af083e2754f69eaf8581b218d1
MikaelSnt <mikael.pinheiro@dcomp.ufs.br>
2026-06-22 21:19:53 -0300

    Adicionando as Pastas MVC

--- Atividade_2(teste)/Desenhos.py
+++ Atividade_2(teste)/Desenhos.py
@@ -3,25 +3,16 @@ from Figuras import *
 def desenhar_poligono(self, event):
     self.pontos_poligono.append(event.x)
     self.pontos_poligono.append(event.y)
-    
-    coordenadas = ((int(self.menu_poly.get())) +1) * 2
-    
+
+    coordenadas = int(self.menu_poly.get()) * 2
+
     if len(self.pontos_poligono) < coordenadas:
-        return
-    
-    pontos_finais = self.pontos_poligono[:-2]
+            return
 
-    figura_atual = Poligono(
-        self.cor_borda.get(),
-        float(self.espessura.get()),
-        self.cor_preenchimento.get(),
-        pontos_finais.copy()
-    )
-
+    figura_atual = Poligono(self.cor_borda.get(), float(self.espessura.get()), self.cor_preenchimento.get(), self.pontos_poligono.copy())
     self.figuras.append(figura_atual)
     self.pontos_poligono.clear()
     self.redesenhar()
-
 def redesenhar(self):
 
     self.canvas.delete("all")
